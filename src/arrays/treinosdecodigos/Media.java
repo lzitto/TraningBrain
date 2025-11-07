5debd3c3238c0f22cdc9f01d1ce27631b11e681a
lzitto <david.leonam.holanda@ccc.ufcg.edu.br>
2025-11-07 11:43:23 -0300

    Lets

---
+++ src/arrays/treinosdecodigos/Media.java
@@ -0,0 +1,39 @@
+package arrays.treinosdecodigos;
+
+public class Media {
+    public static void main(String[] args) {
+         
+        somaExt(a,0,a.length-1, m);
+    }
+    
+
+    
+    public void media(double[] a, double[] m){
+        for(int i = 0; i < a.length; i++) {
+            double soma = 0;
+            for(int j = 0;j<i;j++){
+                soma = soma + a[j];
+            }
+            m[i] = soma/(i+1);
+        }
+    }
+    
+    public double somaInt(double[] a, int l, int r){
+        double soma = 0;
+        if(l == r) {
+            soma = a[l];
+            soma = a[l] + somaInt(a, l+1, r);
+        }
+        return soma;
+    }
+
+    public void somaExt(double[] a, int l, int r, double[] m) {
+        if(l<=r){
+            double somaTemp = somaInt(a,l,r);
+            m[r] = somaTemp/(r+1);
+            somaExt(a,l,r-1,m); 
+        }
+    }
+}
+
+

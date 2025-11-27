15ad0c57cd20019fa89bcd702edd6f4d8737eeb7
lzitto <davidleonam10@gmail.com>
2025-11-26 21:39:02 -0300

    bee1016 lvL 16

---
+++ src/beCrowdTrain/BeC1016.java
@@ -0,0 +1,14 @@
+package beCrowdTrain;
+import java.util.Scanner;
+
+public class BeC1016 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);  
+
+        int km = sc.nextInt();
+        int dist = Math.abs(km * 2);
+
+        System.out.println(dist);
+
+    }
+}

79c2e1213a01b63bcba97980e377b8fdecbd366c
lzitto <davidleonam10@gmail.com>
2025-12-04 03:32:03 -0300

    Ok NAO BRINCA TA

---
+++ src/beCrowdTrain/BeC1017.java
@@ -0,0 +1,23 @@
+package beCrowdTrain;
+import java.util.Scanner;
+
+
+// OPa, QUESTAO DE CUSTO DE COMBUSTIVEL EU GOSTO MUITO! VAMOS FAZEZR 
+public class BeC1017 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);  
+
+        // tempo e velocidade media 
+        // saida > Litros que gastou
+
+        int kml = 12;
+        int tempo = sc.nextInt();
+        int velc = sc.nextInt();
+
+        double litors = tempo * velc / kml;
+
+        System.out.println();
+    
+
+    }
+}

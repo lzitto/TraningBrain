5ecbfcd3e7d38241c7bacb7b54eeb270fb9175c6
lzitto <davidleonam10@gmail.com>
2026-05-07 11:47:46 -0300

    att: Treino

---
+++ src/main/leetcode/Solution.java
@@ -0,0 +1,17 @@
+package leetcode;
+public class Solution {
+    
+    //public static int[] getConcatenation(int[] nums) {
+        // logica aaqui
+    //}
+
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,3};
+
+        //int[] resultado = getConcatenation(nums);
+
+        visualizarArray(nums);
+    }
+}
+

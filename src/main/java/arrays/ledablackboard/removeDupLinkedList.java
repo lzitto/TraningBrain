fdc4e75a2e29dc3a82057555a410e70c0cacacc2
lzitto <davidleonam10@gmail.com>
2026-06-18 21:56:12 -0300

    mister Slinter

---
+++ src/main/java/arrays/ledablackboard/removeDupLinkedList.java
@@ -0,0 +1,12 @@
+package arrays.ledablackboard;
+
+public class removeDupLinkedList {
+    public static void main(String[] args) {
+        
+        // fazer remoção dup recursiva. remoção recursiva remove todos os pares e deixa todos os impares
+        // o baguilho e codar brincando com as listas. e antes de lista/
+        // temos vector. ai filas e pilhas, agora vamo brincar de jogo da cobrinha!! sera q foi assim q susgiu ?
+        
+        int[] array = {}
+    }
+}

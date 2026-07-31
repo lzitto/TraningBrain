4d1f456e9a204727bd3e85142ef50cd0d9bfb326
lzitto <davidleonam10@gmail.com>
2026-07-31 08:32:10 -0300

    fe

--- src/main/java/leetcode/MainDebugger.java
+++ src/main/java/leetcode/MainDebugger.java
@@ -1,14 +1,27 @@
 package leetcode;
 
-import java.util.Arrays;
-
 public class MainDebugger {
 
     public static void main(String[] args) {
 
-        int[] arr = { 3, 2, 6, 8, 1 };
-        System.out.println(Arrays.toString(arr));
-        leetcode.ArrayFun.bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
+        // 1. Monta os nós na mão (BST)
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        // 2. Instancia a sua Solution
+        Solution solua = new Solution();
+
+        // 3. Roda a busca
+        TreeNode resultado = solua.searchBST(root, 2);
+
+        // 4. Imprime a saída
+        if (resultado != null) {
+            System.out.println("Nó encontrado: " + resultado.val);
+        } else {
+            System.out.println("null");
+        }
     }
 }
